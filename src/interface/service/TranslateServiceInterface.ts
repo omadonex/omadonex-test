c3e06981d013b6d3a1d9d64cfc2dc280ac2666ca
omadonex <omadonex@yandex.ru>
2022-12-02 17:49:23 +0300

    0.0.1

--- src/interface/service/TranslateServiceInterface.ts
+++ src/interface/service/TranslateServiceInterface.ts
@@ -1,4 +1,4 @@
 export interface TranslateServiceInterface {
-    t(key: string, replace: object, lang?: string): string;
-    tValidate(rule: string, replace: object, lang?: string): string;
+  t(key: string, replace: object, lang?: string): string;
+  tValidate(rule: string, replace: object, lang?: string): string;
 }
